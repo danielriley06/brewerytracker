4b428f3f5e9c3364b098bafb80b7aaee14bfb7c4
danielriley06 <driley3@gmail.com>
2016-06-06 15:40:18 -0400

    added breweries route to test user login/logout

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,3 +1,12 @@
 class BreweriesController < ApplicationController
 
+  get '/breweries' do
+      if session[:user_id]
+        @breweries = Breweries.all
+        erb :'breweries/breweries'
+      else
+        redirect to '/login'
+      end
+    end
+
 end
